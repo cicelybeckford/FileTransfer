ee5176f3de076e0926dc1d5ae8156b3e4aa0aeec
Sean-HU <32807155+Sean-HU@users.noreply.github.com>
2019-04-24 20:09:41 -0700

    Add files via upload

--- client.py
+++ client.py
@@ -1,24 +1,34 @@
 import socket
 
-TCP_IP = 'fdef:7597:b371:0:54a7:2b5d:1747:d3aa'
+#TCP_IP = 'fdef:7597:b371:0:54a7:2b5d:1747:d3aa'
+TCP_IP = 'localhost'
 TCP_PORT = 7777
 BUFFER_SIZE = 1024
 
 s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
+
+filename = "mytexbbcccct.txt"
+s.send('%s\n' % filename)
+
 with open('received_file', 'wb') as f:
-    print 'file opened'
     while True:
         #print('receiving data...')
         data = s.recv(BUFFER_SIZE)
-        print('data=%s', (data))
-        if not data:
-            f.close()
-            print 'file close()'
+        if data == '-1':
+            output = 'Error: File not found'
             break
-        # write data to a file
-        f.write(data)
+        else:
+            print 'file opened'
+            print('data=%s' % data)
+            output = 'Successfully got the file'
+            if not data:
+                f.close()
+                print 'file close()'
+                break
+            # write data to a file
+            f.write(data)
 
-print('Successfully get the file')
+print(output)
 s.close()
 print('connection closed')
--- server.py
+++ server.py
@@ -1,32 +1,40 @@
 import socket
 from threading import Thread
+import os
 from SocketServer import ThreadingMixIn
 
-TCP_IP = 'fdef:7597:b371:0:56dc:7907:ee4a:7a64'
+#TCP_IP = 'fdef:7597:b371:0:56dc:7907:ee4a:7a64'
+TCP_IP = 'localhost'
 TCP_PORT = 7777
 BUFFER_SIZE = 1024
 
 class ClientThread(Thread):
 
-    def __init__(self,addr,sock):
+    def __init__(self,addr,sock,filename):
         Thread.__init__(self)
         self.addr = addr
         self.sock = sock
+        self.filename = filename
         print " New thread started for " + str(addr)
 
     def run(self):
-        filename='mytext.txt'
-        f = open(filename,'rb')
-        while True:
-            l = f.read(BUFFER_SIZE)
-            while (l):
-                self.sock.send(l)
-                #print('Sent ',repr(l))
+        exists = os.path.isfile(self.filename)
+        if exists:
+            f = open(self.filename,'rb')
+            while True:
                 l = f.read(BUFFER_SIZE)
-            if not l:
-                f.close()
-                self.sock.close()
-                break
+                while (l):
+                    self.sock.send(l)
+                    # print('Sent ',repr(l))
+                    l = f.read(BUFFER_SIZE)
+                if not l:
+                    f.close()
+                    self.sock.close()
+                    break
+        else:
+            self.sock.send("-1")
+            self.sock.close()
+
 
 tcpsock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -39,7 +47,13 @@ while True:
     #(conn, (ip,port)) = tcpsock.accept()
     conn, addr = tcpsock.accept()
     print 'Got connection from ', addr
-    newthread = ClientThread(addr,conn)
+    filename = ""
+    while True:
+        data = conn.recv(1)
+        if data == '\n':
+            break
+        filename += data
+    newthread = ClientThread(addr, conn, filename)
     newthread.start()
     threads.append(newthread)
 
